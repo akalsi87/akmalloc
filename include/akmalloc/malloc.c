383fb2b41cdfde739379e3c7f955dc1eb0ddd268
Aaditya Kalsi <akalsi.dev@gmail.com>
2016-04-08 19:06:39 -0400

    Update malloc.c

--- include/akmalloc/malloc.c
+++ include/akmalloc/malloc.c
@@ -37,6 +37,44 @@
  * IMPLEMENTATION
  ***********************************************/
 
+void* ak_malloc(size_t sz)
+{
+    return 0;
+}
 
+void* ak_calloc(size_t elsz, size_t numel)
+{
+    return 0;
+}
+
+void ak_free(void* mem)
+{
+
+}
+
+void* ak_aligned_alloc(size_t sz, size_t aln)
+{
+    return 0;
+}
+
+int ak_posix_memalign(void** pmem, size_t sz, size_t aln)
+{
+    return 0;
+}
+
+void* ak_memalign(size_t sz, size_t aln)
+{
+    return 0;
+}
+
+void* ak_realloc(void* mem, size_t newsz)
+{
+    return 0;
+}
+
+size_t ak_malloc_usable_size(const void* mem)
+{
+    return 0;
+}
 
 #endif/*AKMALLOC_MALLOC_C*/
