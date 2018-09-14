bedcdc2ced92c2bc1bde46fe9e4e743ebf6f56be
Aaditya Kalsi <akalsi.dev@gmail.com>
2018-09-14 19:39:33 -0400

    Add helper scripts

--- build.sh
+++ build.sh
@@ -6,12 +6,13 @@ if [ "$OS" != "" ]; then
     gen="'Visual Studio 15 Win64'"
     cflags=""
     cxxflags=""
+    export AKMALLOC_LINK_STATIC=1
 else
     gen=""
     cflags="-g"
     cxxflags="-std=c++11 -Wno-unused-variable -pthread"
+    export AKMALLOC_LIBRARY=1
 fi
 
-env CFLAGS="$cflags" CXXFLAGS="$cxxflags" \
-    VERBOSE=1 AKMALLOC_LINK_STATIC=1 \
+env CFLAGS="$cflags" CXXFLAGS="$cxxflags" VERBOSE=1 \
     bash -c "tools/build $1 $gen"
